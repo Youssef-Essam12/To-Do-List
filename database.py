f5f6089ca700831b1df76096385a3097596b2c6d
Youssef Essam <98584040+Youssef-Essam12@users.noreply.github.com>
2026-02-06 13:52:21 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-
-DATABASE_URL = "postgresql://todo_user:todo@db:5432/todo_db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://todo_user:todo@localhost/todo_db")
+# DATABASE_URL = "postgresql://todo_user:todo@db:5432/todo_db"
 
 engine = create_engine(DATABASE_URL)
 
